f785972b147e0eabbeaad560222560cd805d556e
heer-vaishnav <48050788+heer-vaishnav@users.noreply.github.com>
2020-03-27 15:01:10 +0530

    Console Application

---
+++ Inventory.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductInventory
+{
+    public class Inventory
+    {
+        int price;
+        string productName;
+        Dictionary<string, int> product = new Dictionary<string, int>();
+
+        public Inventory(string productName,int price)
+        {
+            this.productName = productName;
+            this.price = price;
+        }
+
+        public void List()
+        {
+            product.Add("TV", 2000);
+            product.Add("Shampoo", 150);
+            foreach (KeyValuePair<string, int> item in product)
+            {
+                Console.WriteLine("{0}-{1}",
+                          item.Key, item.Value);
+            }
+
+            Console.WriteLine("");
+        }
+
+        public void AddNewProduct()
+        {
+            //Dictionary<string, string> Users = new Dictionary<string, string>();
+            Console.WriteLine("How many products you want to add?:");
+            int n = Int32.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Enter Product Name :");
+                productName = Console.ReadLine();
+                Console.WriteLine("Enter Price:");
+                price = Int32.Parse(Console.ReadLine());
+
+                product.Add(productName, price);
+                //Console.WriteLine("Your Product is " + productName + " and Price is " + price);
+                foreach (KeyValuePair<string, int> item in product)
+                {
+                    Console.WriteLine("{0}-{1}",
+                              item.Key, item.Value);
+                }
+                Console.WriteLine("");
+
+            }
+           //Console.WriteLine(product.Count);
+        }
+
+        public void Search()
+        {
+            Console.WriteLine("Enter ProductName you want to search:");
+
+            string prod = Console.ReadLine();
+            if (product.ContainsKey("" + prod))
+            {
+                Console.WriteLine("Product Found :" + prod);
+            }
+            else
+            {
+                Console.WriteLine("Product is not Available");
+            }
+            Console.WriteLine("");
+        }
+
+
+        public void Count()
+        {
+            Console.WriteLine("Total Products:" + product.Count);
+        }  
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ProductInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Inventory inventory = new Inventory("Mobile",1000);
+        lable:
+            Console.WriteLine("Hello...Welcome To The Store!!!");
+            Console.WriteLine("Enter Your Choice:");
+            Console.WriteLine("1.View Products");
+            Console.WriteLine("2.Add Product");
+            Console.WriteLine("3.Search");
+            Console.WriteLine("4.Count");
+            Console.WriteLine("5.Exit");
+        
+            int ch = Int16.Parse(Console.ReadLine());
+            switch (ch)
+            {
+                case 1:
+                    inventory.List();
+                    //Console.WriteLine("UserName And Password are:");
+                    goto lable;
+
+                case 2:
+                    inventory.AddNewProduct();
+                    //Console.WriteLine("Deposit"+acc);
+                    goto lable;
+
+                case 3:
+                    inventory.Search();
+                    // Console.WriteLine("Deposit" + acc);
+                    goto lable;
+
+                case 4:
+                    inventory.Count();
+                    // Console.WriteLine("Deposit" + acc);
+                    goto lable;
+
+                case 5:
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid!!");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
